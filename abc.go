4aa617e88c575f18e24e6ab3d3c2f4e112cf54b8
matteo <matteo-gz@users.noreply.github.com>
2022-12-14 19:21:03 +0800

    code

--- abc.go
+++ abc.go
@@ -6,16 +6,23 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"strings"
 )
 
 func main() {
-	ReadFile("in.txt")
+	ReadFile("in.txt", "out.txt")
+	ReadFile2("in2.txt", "out2.txt")
 }
-func ReadFile(filename string) {
+func ReadFile(filename, outName string) {
 	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
+	file2, err := os.OpenFile(outName, os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file2.Close()
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	i := 1
@@ -29,15 +36,62 @@ func ReadFile(filename string) {
 		} else if Replace2(&s, &i) {
 
 		}
-		fmt.Println(s)
-		//break
+		_, err = file2.WriteString(s + "\n")
+		if err != nil {
+			log.Println(err)
+		}
+
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 }
+func ReadFile2(filename, outName string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file2, err := os.OpenFile(outName, os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file2.Close()
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		s := scanner.Text()
+
+		if Replace4(&s) {
+
+		}
+		_, err = file2.WriteString(s)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+func Replace4(str *string) bool {
+	reg := regexp.MustCompile(`[*]([a-zA-Z0-9.]+)\s?`)
+	s := reg.FindString(*str)
+	if s != "" {
+		tpl := `
+if %s!=nil{
+%s
+}`
+		s = strings.TrimSpace(s)
+		s = s[1:]
+		*str = fmt.Sprintf(tpl, s, *str)
+		return true
+	}
+
+	return false
+}
 func Replace(str *string) bool {
-	reg := regexp.MustCompile(`[a-zA-Z0-9]+\s\{`)
+	reg := regexp.MustCompile(`[a-zA-Z0-9]+\s?\{`)
 	s := reg.FindString(*str)
 	//fmt.Println("---", *str, "---")
 	if s != "" {
